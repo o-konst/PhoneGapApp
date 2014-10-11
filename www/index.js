fa6d896062b16822811ba47f73eb370691c2cf38
o-konst <ogay.kt@gmail.com>
2014-10-11 20:53:21 +1300

    test

--- www/index.js
+++ www/index.js
@@ -42,10 +42,26 @@ var app = {
    
     // deviceready Event Handler
     onDeviceReady: function() {
-        //alert(1+device.platform);
-        angular.bootstrap(document.body, ["digestApp"]);
-        /*angular.element(document).ready(function() {
-            angular.bootstrap(document);
-        });*/
+        //angular.bootstrap(document.body, ["digestApp"]);
+         var onCaptureSuccess = function(data){
+   
+
+    //$scope.project.template.placeholders[0].photo.url =data;
+    //$rootScope.$apply();
+    //$scope.testurl = data;
+     var image = document.getElementById('myImage');
+    //alert(image);
+   image.src = data;
+ //console.log("image ulr>> " + data);
+    }
+
+    var onCaptureFail = function(){
+
+    }
+
+  navigator.camera.getPicture(onCaptureSuccess, onCaptureFail, {
+    destinationType: Camera.DestinationType.FILE_URI,
+    sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+});
     }
 };
