6808e8acfc8de523e9b45d41c69a69513a0a9366
o-konst <ogay.kt@gmail.com>
2014-10-14 21:25:58 +1300

    test

--- www/scripts/controllers/preview.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('photoCardsApp')
-  .controller('PreviewCtrl', ['$scope', '$routeParams' ,'datafactory',  function ($scope, $routeParams, datafactory) {
-    var cardid = $routeParams.cardid;
-    
-$scope.card = _.findWhere(datafactory.myCardsCollection, {id:parseInt(cardid)});
-console.log($scope.card)
-  }]);
